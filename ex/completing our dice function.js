d3a3fefc4e06b1834f2f32158c2ce428e883e876
Ayush Tiwari <64984686+ayushballia@users.noreply.github.com>
2022-02-09 20:14:02 +0530

    Delete ex directory

--- ex/completing our dice function.js
+++
@@ -1,7 +0,0 @@
-// Create a function , rollDice(), that returns a random number between 1 to 6
-
-function rollDice() {
-  let randomNumber = Math.floor(Math.random() * 6 + 1);
-  return randomNumber;
-}
-console.log(rollDice());
